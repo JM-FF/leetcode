796c9035fd39d83053576cc35a1c831e743a4d64
JM-FF <jm9992528@163.com>
2020-06-30 22:07:38 +0800

    剑指 Offer 10- II. 青蛙跳台阶问题

---
+++ 青蛙问题.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+	int numWays(int n) {
+		vector<int> dp(n+2);
+		dp[0] = 1;
+		dp[1] = 1;
+		for (int i = 2; i <= n; i++)
+		{
+			dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+		}
+		return	dp[n];
+	}
+};
