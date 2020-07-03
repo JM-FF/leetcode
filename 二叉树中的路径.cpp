88e7ff7ceb0254e4382a37db8d1114a378b609e0
JM-FF <jm9992528@163.com>
2020-07-03 16:52:15 +0800

    剑指 Offer 34. 二叉树中和为某一值的路径

--- 二叉树中的路径.cpp
+++ 二叉树中的路径.cpp
@@ -25,3 +25,42 @@ private:
 		cur -= root->val;
 	}
 };
+
+
+//增加判断 cur>sum时换路径
+class Solution {
+public:
+	vector<vector<int>> pathSum(TreeNode* root, int sum) {
+		vector<int> tmp;
+		vector<vector<int>> result;
+		int cur = 0;
+		dfs(root, sum, cur, tmp, result);
+		return result;
+	}
+
+private:
+	void dfs(TreeNode* root, int sum, int& cur, vector<int> &tmp,vector<vector<int>> &result)
+	{
+		if (!root)
+			return;
+		tmp.push_back(root->val);
+		cur += root->val;
+		if (cur > sum&&root->left == nullptr&&root->right == nullptr)
+		{
+			cur -= root->val;
+			tmp.pop_back();
+			return;
+		}
+		else if (cur == sum&&root->left == nullptr&&root->right == nullptr)
+		{
+			cur -= root->val;
+			result.push_back(tmp);
+			tmp.pop_back();
+			return;
+		}
+		dfs(root->left, sum, cur, tmp, result);
+		dfs(root->right, sum, cur, tmp, result);
+		tmp.pop_back();
+		cur -= root->val;
+	}
+};
