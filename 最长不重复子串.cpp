cffc9b231395df78d7854e50f9e486196f56923f
JM-FF <jm9992528@163.com>
2020-07-04 21:52:43 +0800

    剑指 Offer 48. 最长不含重复字符的子字符串

---
+++ 最长不重复子串.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+	int lengthOfLongestSubstring(string s) {
+		unordered_set<char> tmp;
+		int maxlen = 0;
+		for (int i = 0;i < s.size();i++)
+		{
+			int len = 0;
+            tmp.clear();
+			tmp.insert(s[i]);
+			len++;
+			for (int j = i + 1;j < s.size();j++)
+			{
+				if (tmp.count(s[j]))
+				{
+					break;
+				}
+				else
+				{
+					tmp.insert(s[j]);
+					len++;
+				}
+			}
+			if (len > maxlen)
+				maxlen = len;
+		}
+		return maxlen;
+	}
+};
