6fe2d7615d5f8e2582d124414234e1caeb8a0502
JM-FF <jm9992528@163.com>
2020-08-11 00:02:55 +0800

    350. 两个数组的交集 II

---
+++ 两个数组的交集.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int,int> tmp;
+        vector<int> res;
+        if(nums1.size()>nums2.size())
+        {
+            for(auto m : nums1)
+            {
+                tmp[m]++;
+            }
+            for(auto m:nums2)
+            {
+                if(tmp[m]>0)
+                {
+                    res.push_back(m);
+                    tmp[m]--;
+                }
+            }
+        }
+        else
+        {
+            for(auto m : nums2)
+            {
+                tmp[m]++;
+            }
+            for(auto m:nums1)
+            {
+                if(tmp[m]>0)
+                {
+                    res.push_back(m);
+                    tmp[m]--;
+                }
+            }
+        }
+        return res;
+    }
+};
